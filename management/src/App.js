d1f523f131bf3f36d9258003fef37bbb6952d8bb
limyoungoong_Home_desktop <tywowie@gmail.com>
2021-04-02 23:38:19 +0900

    5강 - 고객 컴포넌트 구조화하기

--- management/src/App.js
+++ management/src/App.js
@@ -3,24 +3,56 @@ import Customer from './components/Customer';
 import './App.css';
 
 function App() {
-  const customer ={
+  const customers =[
+  {
+    'id':'1',
+    'image':'https://placeimg.com/64/64/1',
     'name':'임영웅',
     'birthday':'900729',
     'gender':'남자',
     'job' : '직장인'
 
-  }
+  },
+  {
+    'id':'2',
+    'image':'https://placeimg.com/64/64/2',
+    'name':'성시경',
+    'birthday':'900729',
+    'gender':'남자',
+    'job' : '직장인'
 
+  },{
+    'id':'3',
+    'image':'https://placeimg.com/64/64/3',
+    'name':'최민수',
+    'birthday':'900729',
+    'gender':'남자',
+    'job' : '직장인'
+
+  }
+    
+  ]
 
   return (
-    <Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job ={customer.job}
-    
-    ></Customer>
+    <div>
+      {customers.map(c =>{
+            return(
+              <Customer 
+                key ={c.id}
+                id = {c.id}
+                image = {c.image}
+                name ={c.name}
+                birthday = {c.birthday}
+                gender = {c.gender}
+                job = {c.job}
+              />
+            )
+        })
+      }
+    </div>
   );
 }
+
+
 
 export default App;
