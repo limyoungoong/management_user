75e92afbb0d37b1b45d98b811020b26eca53e39d
DESKTOP-2BHUKQ3\tywow <tywowie@gmail.com>
2021-04-03 15:58:16 +0900

    material Design 추가

--- management/server.js
+++ management/server.js
@@ -6,8 +6,35 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message : 'Hello world!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id':'1',
+            'image':'https://placeimg.com/64/64/1',
+            'name':'임영웅',
+            'birthday':'900729',
+            'gender':'남자',
+            'job' : '직장인'
+        
+        },
+        {
+            'id':'2',
+            'image':'https://placeimg.com/64/64/2',
+            'name':'김민성',
+            'birthday':'900729',
+            'gender':'남자',
+            'job' : '변호사'
+        
+        },{
+            'id':'3',
+            'image':'https://placeimg.com/64/64/3',
+            'name':'최민수',
+            'birthday':'900729',
+            'gender':'남자',
+            'job' : '직장인'
+        
+        }
+    ]);
 });
 
 app.listen(port, ()=> console.log(`listening on port ${port}`));
