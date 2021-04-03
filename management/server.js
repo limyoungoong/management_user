bf3e3fd667e8a423edcc0fc25b9b13223fc02451
DESKTOP-2BHUKQ3\tywow <tywowie@gmail.com>
2021-04-03 20:37:14 +0900

    11강 - 고객(Customer) DB 테이블 구축 및 Express와 연동하기 [React와 Node.js를 활용한 고객 관리 시스템 개발 강의]

--- management/server.js
+++ management/server.js
@@ -1,40 +1,33 @@
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database : conf.database
+});
+
+connection.connect();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id':'1',
-            'image':'https://placeimg.com/64/64/1',
-            'name':'임영웅',
-            'birthday':'900729',
-            'gender':'남자',
-            'job' : '직장인'
-        
-        },
-        {
-            'id':'2',
-            'image':'https://placeimg.com/64/64/2',
-            'name':'김민성',
-            'birthday':'900729',
-            'gender':'남자',
-            'job' : '변호사'
-        
-        },{
-            'id':'3',
-            'image':'https://placeimg.com/64/64/3',
-            'name':'최민수',
-            'birthday':'900729',
-            'gender':'남자',
-            'job' : '직장인'
-        
+    connection.query(
+        "select * from customer",
+        (err,rows,fields) => {
+            res.send(rows);
         }
-    ]);
+    )
 });
 
 app.listen(port, ()=> console.log(`listening on port ${port}`));
